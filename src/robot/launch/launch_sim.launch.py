9a33c54814cfcf4521d2d350a8d9732960a5d19c
varsenii <varsenyi@gmail.com>
2024-04-06 21:37:04 +0200

    Update Gazebo clock rate

--- src/robot/launch/launch_sim.launch.py
+++ src/robot/launch/launch_sim.launch.py
@@ -14,6 +14,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     package_name = 'diff_drive_bot'
     robot_name = 'robot'
+    gazebo_params_file = os.path.join(
+        get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -25,6 +27,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' +  gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package
