016cfe649498c99c598009f2231b85b0ac79d78b
varsenii <varsenyi@gmail.com>
2024-04-08 21:30:20 +0200

    Swapping between control methods

--- src/robot/launch/launch_sim.launch.py
+++ src/robot/launch/launch_sim.launch.py
@@ -4,7 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -14,13 +16,16 @@ from launch_ros.actions import Node
 def generate_launch_description():
     package_name = 'diff_drive_bot'
     robot_name = 'robot'
+
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
     gazebo_params_file = os.path.join(
         get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': use_ros2_control}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -40,17 +45,23 @@ def generate_launch_description():
     diff_drive_spawner = Node(
         package='controller_manager', 
         executable='spawner',
-        arguments=['diff_cont']
+        arguments=['diff_cont'],
+        condition=IfCondition(use_ros2_control)
     )
 
     # Run the spawner node to start the joint_broad controller
     joint_broad_spawner = Node(
         package='controller_manager', 
         executable='spawner',
-        arguments=['joint_broad']
+        arguments=['joint_broad'],
+        condition=IfCondition(use_ros2_control)
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
         rsp,
         gazebo,
         spawn_entity,
